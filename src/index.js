1ed255546175d99cf9b238ec0e70e4cfda067fb1
Maxime Ghignet <maxime.ghignet@gmail.com>
2017-01-08 17:17:17 +0100

    animate the car, changing its color

--- src/index.js
+++ src/index.js
@@ -14,11 +14,44 @@ function drawCar() {
 `
 }
 
+function setColor(car, color) {
+  const resetColor = '\x1b[0m'
+  return color + car + resetColor
+}
+
+function getCarHeight(car) {
+  return car.split(/\r\n|\r|\n/).length - 1
+}
+
+function animateCar(car) {
+  const colors = [
+    '\x1b[31m',
+    '\x1b[32m',
+    '\x1b[33m',
+    '\x1b[34m',
+    '\x1b[35m',
+    '\x1b[36m',
+    '\x1b[37m',
+  ]
+
+  let colorIndex = 0
+  process.stdout.write('\r' + car)
+
+  return setInterval(function () {
+    process.stdout.moveCursor(0, -getCarHeight(car));
+    process.stdout.clearScreenDown()
+
+    const coloredCar = setColor(car, colors[colorIndex++])
+    process.stdout.write(coloredCar)
+
+    colorIndex = colorIndex % colors.length
+  }, 100);
+}
 
 console.log('Hello guys,')
 console.log('Look at my car!')
 console.log('...')
 
 const car = drawCar()
-console.log(car)
+animateCar(car)
 
