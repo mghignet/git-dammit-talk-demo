7516980f3441d1f0871176830f0995c50960f8f4
Maxime Ghignet <maxime.ghignet@gmail.com>
2017-01-08 14:41:19 +0100

    draw a car

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 'use strict'
 
+
+function drawCar() {
+	return `
+    ▄▄▄▄▄▄
+  ▄█▓▓██▓▓█▄
+▄██████████████▓
+▀█ ▄▄ ████ ▄▄ ██
+   ▀▀      ▀▀`
+}
+
+
 console.log('Hello guys,')
 console.log('Look at my car!')
 console.log('...')
+
+const car = drawCar()
+console.log(car)
+
