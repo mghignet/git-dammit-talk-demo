4781889b3571e86215a41d67deb9fe612c36f5b4
Maxime Ghignet <maxime.ghignet@gmail.com>
2017-01-17 00:33:04 +0100

    didn't need that

--- a.js
+++ a.js
@@ -12,5 +12,12 @@ a
 a
 a
 a
+<<<<<<< HEAD
 >>>>>>> d3a07e6... forgot a space
+<<<<<<< HEAD
 >>>>>>> d292dd4... forgot a space
+=======
+=======
+a
+>>>>>>> aa9aa3f... didn't need that
+>>>>>>> ee416f2... didn't need that
